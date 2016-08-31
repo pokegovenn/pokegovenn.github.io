4c87e8fc6fc224f39feb8f80110450ba50212be4
evanthebouncy <evanthebouncy@gmail.com>
2016-08-31 11:02:03 -0400

    fk

--- script.js
+++ script.js
@@ -21,6 +21,9 @@
         }
       };
 
+      // ============== CREATING BUTTONS ============
+
+      // function to create toggle green btn
       function CenterControlBtnGreen(controlDiv, map) {
 
         // Set CSS for the control border.
@@ -43,14 +46,45 @@
         controlText.style.lineHeight = '38px';
         controlText.style.paddingLeft = '5px';
         controlText.style.paddingRight = '5px';
-        controlText.innerHTML = 'Center Map';
+        controlText.innerHTML = 'Color Green';
         controlUI.appendChild(controlText);
 
         // Setup the click event listeners: simply set the map to Chicago.
         controlUI.addEventListener('click', function() {
           map.setCenter({lat: 41.85, lng: -87.65});
         });
+      }
 
+      // function to create toggle red btn
+      function CenterControlBtnRed(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to recenter the map';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Color Red';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Chicago.
+        controlUI.addEventListener('click', function() {
+          map.setCenter({lat: 41.85, lng: -87.65});
+        });
       }
 
 
@@ -66,7 +100,8 @@
         // Create the DIV to hold the control and call the CenterControl()
         // constructor passing in this DIV.
         var centerControlDiv = document.createElement('div');
-        var centerControl = new CenterControlBtnGreen(centerControlDiv, map);
+        var centerControlGreen = new CenterControlBtnGreen(centerControlDiv, map);
+        var centerControlRed = new CenterControlBtnRed(centerControlDiv, map);
 
         centerControlDiv.index = 1;
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
